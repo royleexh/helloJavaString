446ba95a7b6730765081dff3d145695448401908
royleexh <271173141@qq.com>
2013-02-04 10:06:32 +0800

    aa

--- src/com/test/SampleStringTest.java
+++ src/com/test/SampleStringTest.java
@@ -3,6 +3,27 @@ package com.test;
 import junit.framework.TestCase;
 
 public class SampleStringTest extends TestCase{
-
-
+	
+	private int a;
+	private int b;
+	private int r1,r2;
+	
+	protected void setUp() {
+		a = 50;
+		b = 20;
+		r1 = 70;
+		r2 = 30;
+	}
+	
+	protected void tearDown(){
+		
+	}
+	
+	public void testAdd(){
+		assertEquals(r1 , a+b);/*¼ì²â²âÊÔ½á¹û*/
+	}
+	
+	public void testSubtration(){
+		assertEquals(r2 , a-b);/*¼ì²â²âÊÔ½á¹û*/
+	}
 }
